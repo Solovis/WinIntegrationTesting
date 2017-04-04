de7cb5f8ddbf9c3d77f667ef70d886c8382fe407
Caleb Doise <Caleb Doise>
2017-04-04 12:40:48 -0400

    add start of unit test project

---
+++ tests/WinIntegrationTestingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinIntegrationTestingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinIntegrationTestingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f5904f04-8a89-42d4-a241-7415d784c9ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tests/WinIntegrationTestingTests/VisualStudioHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinIntegrationTesting;
+using System.IO;
+
+namespace WinIntegrationTestingTests
+{
+    [TestClass]
+    public class VisualStudioHelperTests
+    {
+        [TestMethod]
+        public void TestFindSolutionFolderAndFileForAssembly()
+        {
+            string solutionFolder = VisualStudioHelper.FindSolutionFolderForAssembly(typeof(VisualStudioHelperTests).Assembly);
+            string solutionFile = VisualStudioHelper.FindSolutionFileForAssembly(typeof(VisualStudioHelperTests).Assembly);
+
+            string visualStudioHelperTestsFile = Path.Combine(solutionFolder, "tests", "WinIntegrationTestingTests", "VisualStudioHelperTests.cs");
+            Assert.IsTrue(File.Exists(visualStudioHelperTestsFile));
+
+            string expectedSolutionFile = Path.Combine(solutionFolder, "WinIntegrationTesting.sln");
+            Assert.AreEqual(expectedSolutionFile, solutionFile);
+        }
+
+        [TestMethod]
+        public void TestFindSolutionFolderAndFileForSubFolder()
+        {
+            string solutionFolder = VisualStudioHelper.FindSolutionFolderForAssembly(typeof(VisualStudioHelperTests).Assembly);
+            string solutionFile = VisualStudioHelper.FindSolutionFileForAssembly(typeof(VisualStudioHelperTests).Assembly);
+
+            string testProjectFolder = Path.Combine(solutionFolder, "tests", "WinIntegrationTestingTests");
+
+            string solutionFolderForSubFolder = VisualStudioHelper.FindSolutionFolderForSubFolder(testProjectFolder);
+            Assert.AreEqual(solutionFolder, solutionFolderForSubFolder);
+
+            string solutionFileForSubFolder = VisualStudioHelper.FindSolutionFileForSubFolder(testProjectFolder);
+            Assert.AreEqual(solutionFile, solutionFileForSubFolder);
+        }
+    }
+}
